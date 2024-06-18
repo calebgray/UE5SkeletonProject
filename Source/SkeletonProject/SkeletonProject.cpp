90550e184ffd6bc5baec1ab42dfebfba224005cf
Caleb Gray <hey@calebgray.com>
2024-06-18 16:14:26 -0700

    Updated to Unreal Engine 5.4.2

--- Source/SkeletonProject/SkeletonProject.cpp
+++ Source/SkeletonProject/SkeletonProject.cpp
@@ -1,6 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #include "SkeletonProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkeletonProject, "SkeletonProject" );
